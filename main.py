1914f51b25a9c81b32ed33ffa8c3ef0bb7d769e5
Rand0m-devnull <curtsy-salmons-0s@icloud.com>
2025-03-23 14:57:43 +0100

    add cell functionality

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-from window import Window, Line, Point
-
+from window import Window
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    point1 = Point(100, 100)
-    point2 = Point(-100, -100)
-    line = Line(point1, point2)
-    win.draw_line(line, "red")
+    cell = Cell(win)
+    cell.has_left_wall = False
+    cell.draw(50, 50, 100, 100)
+
+    cell = Cell(win)
+    cell.has_right_wall = False
+    cell.draw(125, 125, 200, 200)
+
+    cell = Cell(win)
+    cell.has_bottom_wall = False
+    cell.draw(225, 225, 250, 250)
+
+    cell = Cell(win)
+    cell.has_top_wall = False
+    cell.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 if __name__ == "__main__":

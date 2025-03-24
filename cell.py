e7b5ce9a9ddf20391262a8d4acb6cb87818bf585
Rand0m-devnull <curtsy-salmons-0s@icloud.com>
2025-03-24 13:21:26 +0100

    add movement from 1 cell to another

--- cell.py
+++ cell.py
@@ -33,3 +33,9 @@ class Cell():
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        point1 = Point(self._x1 + (abs(self._x2 - self._x1) // 2), self._y1 + (abs(self._y2 - self._y1) // 2))
+        point2 = Point(to_cell._x1 + (abs(to_cell._x2 - to_cell._x1) // 2), to_cell._y1 + (abs(to_cell._y2 - to_cell._y1) // 2))
+        line = Line(point1, point2)
+        self._win.draw_line(line, "gray" if undo else "red")

c6fce70dddaf9df696e63e7b7ee24a0f68bfcda9
Rand0m-devnull <curtsy-salmons-0s@icloud.com>
2025-03-25 15:30:13 +0100

    break walls to move inside a maze

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from maze import Maze
 
 def main():
     win = Window(800, 600)
-    maze = Maze(50, 50, 6, 8, 25, 25, win)
+    maze = Maze(50, 50, 6, 8, 25, 25, win, 10)
 
     win.wait_for_close()
 

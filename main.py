36b9d531990142a12936f5b3247a52c89faabd83
Rand0m-devnull <curtsy-salmons-0s@icloud.com>
2025-03-25 16:46:46 +0100

    solve maze

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from maze import Maze
 def main():
     win = Window(800, 600)
     maze = Maze(50, 50, 6, 8, 25, 25, win, 10)
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("maze can not be solved!")
+    else:
+        print("maze solved!")
 
     win.wait_for_close()
 
